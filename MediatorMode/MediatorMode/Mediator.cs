45c1b2e6dec890e781267b35cad3342068f05d1a
glen1166 <glen1166@outlook.com>
2021-02-06 17:47:00 +0800

    maintain mediator code

--- MediatorMode/MediatorMode/Mediator.cs
+++ MediatorMode/MediatorMode/Mediator.cs
@@ -8,44 +8,57 @@ namespace MediatorMode
 {
     public class Mediator : AbstractMediator
     {
-        override public void execute(string str)
+        public override void execute(string str)
         {
+            var para = 1;
             if (str.Equals("purchase.buy"))
             {
-                
+                buyComputer(para);
             }
             else if (str.Equals("sale.sell"))
             {
-
+                sellComputer(para);
             }
             else if (str.Equals("sale.offsell"))
             {
-
+                offSale();
             }
             else if (str.Equals("stock.clear"))
             {
-
+                clear();
             }
         }
 
         private void buyComputer(int number)
         {
-            
+            int saleStatus = sale.getSaleStatus();
+            if(saleStatus > 80){
+                Console.WriteLine($"{number} computer purchased.");
+                stock.increase(number);
+            }
+            else{
+                int buyNumber = number/2;
+                Console.WriteLine($"{buyNumber}  computer purchased.");
+                stock.increase(buyNumber);
+            }
         }
 
         private void sellComputer(int number)
         {
-            
+            if(stock.getStockNumber()<number){
+                purchase.buyComputer(number);
+            }
         }
 
         private void offSale()
         {
-            
+            Console.WriteLine($"sell computer with cut {stock.getStockNumber()}");
         }
 
         private void clear()
         {
-
+            sale.offSale();
+            purchase.refuseBuyIBM();
         }
     }
 }
